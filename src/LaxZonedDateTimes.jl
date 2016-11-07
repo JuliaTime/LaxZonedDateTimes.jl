5120d781a209c59437ee23add375b20779c32929
Curtis Vogt <curtis.vogt@gmail.com>
2016-11-07 16:41:24 -0600

    Reasonable implementation of LaxZonedDateTime

--- src/LaxZonedDateTimes.jl
+++ src/LaxZonedDateTimes.jl
@@ -1,5 +1,165 @@
 module LaxZonedDateTimes
 
-# package code goes here
+# Prototype of a new type that is a more context aware Nullable{ZonedDateTime}
+
+using TimeZones
+import Base: +, -, .+, .-, ==, show
+import Base.Dates: DatePeriod, TimePeriod, TimeType
+import TimeZones: utc, localtime, timezone, UTC, Local, interpret
+
+export LaxZonedDateTime
+
+abstract InvalidTimeZone <: TimeZone
+
+immutable NonExistent <: InvalidTimeZone
+end
+
+immutable Ambiguous <: InvalidTimeZone
+end
+
+# Seems like we want to keep the UTC datetime even if it doesn't align with our local
+# datetime so that we can still do UTC based calculations.
+
+immutable LaxZonedDateTime <: TimeType
+    local_datetime::DateTime
+    timezone::TimeZone
+    zone::Union{FixedTimeZone,InvalidTimeZone}
+    valid::Bool
+end
+
+function LaxZonedDateTime()
+    utc = TimeZone("UTC")
+    LaxZonedDateTime(DateTime(), utc, utc, false)
+end
+
+function LaxZonedDateTime(zdt::ZonedDateTime)
+    LaxZonedDateTime(localtime(zdt), timezone(zdt), zdt.zone, true)
+end
+
+function LaxZonedDateTime(dt::DateTime, tz::TimeZone, zone)
+    LaxZonedDateTime(dt, tz, zone, true)
+end
+
+function LaxZonedDateTime(local_dt::DateTime, tz::TimeZone)
+    possible = interpret(local_dt, tz, Local)
+
+    num = length(possible)
+    if num == 1
+        lzdt = LaxZonedDateTime(first(possible))
+    elseif num == 0
+        lzdt = LaxZonedDateTime(local_dt, tz, NonExistent())
+    else
+        lzdt = LaxZonedDateTime(local_dt, tz, Ambiguous())
+    end
+
+    return lzdt
+end
+
+function (==)(x::LaxZonedDateTime, y::LaxZonedDateTime)
+    return (
+        x.valid == y.valid == false ||
+        x.local_datetime == y.local_datetime &&
+        x.timezone == y.timezone &&
+        x.zone == y.zone &&
+        x.valid == y.valid
+    )
+end
+
+# function LaxZonedDateTime(local_dt::DateTime, tz::VariableTimeZone)
+#     possible = interpret(local_dt, tz, Local)
+
+#     num = length(possible)
+#     if num == 1
+#         return LaxZonedDateTime(first(possible))
+#     elseif num == 0
+#         before = first(shift_gap(local_dt, tz))
+#         return LaxZonedDateTime(local_dt, Nullable{DateTime}(), tz, Nullable(before.zone))
+#     else
+#         return LaxZonedDateTime(local_dt, Nullable{DateTime}(), tz, Nullable{FixedTimeZone}())
+#     end
+# end
+
+localtime(lzdt::LaxZonedDateTime) = lzdt.local_datetime
+timezone(lzdt::LaxZonedDateTime) = lzdt.timezone
+isvalid(lzdt::LaxZonedDateTime) = lzdt.valid
+
+function utc(lzdt::LaxZonedDateTime)
+    if !isvalid(lzdt)
+        error("Unable to determine UTC datetime from invalid LaxZonedDateTime")
+    end
+
+    if isa(lzdt.zone, FixedTimeZone)
+        return lzdt.local_datetime - lzdt.zone.offset
+    end
+
+    local_dt, tz = localtime(lzdt), timezone(lzdt)
+    possible = interpret(local_dt, tz, Local)
+
+    num = length(possible)
+    if num == 1
+        warn("Internal error")
+        return utc(first(possible))
+    elseif num == 0
+        throw(NonExistentTimeError(local_dt, tz))
+    else
+        throw(AmbiguousTimeError(local_dt, tz))
+    end
+end
+
+(-)(x::LaxZonedDateTime, y::LaxZonedDateTime) = utc(x) - utc(y)
+
+function (+)(lzdt::LaxZonedDateTime, p::DatePeriod)
+    !isvalid(lzdt) && (return lzdt)
+
+    local_dt, tz = localtime(lzdt), timezone(lzdt)
+    local_dt = local_dt + p
+    possible = interpret(local_dt, tz, Local)
+
+    num = length(possible)
+    if num == 1
+        return LaxZonedDateTime(first(possible))
+    elseif num == 0
+        return LaxZonedDateTime(local_dt, tz, NonExistent())
+    else
+        return LaxZonedDateTime(local_dt, tz, Ambiguous())
+    end
+end
+
+function (+)(lzdt::LaxZonedDateTime, p::TimePeriod)
+    !isvalid(lzdt) && (return lzdt)
+
+    if isa(lzdt.zone, InvalidTimeZone)
+        return LaxZonedDateTime()
+    end
+
+    utc_dt, tz = utc(lzdt), timezone(lzdt)
+    possible = interpret(utc_dt + p, tz, UTC)
+    return LaxZonedDateTime(first(possible))
+end
+
+function (-)(lzdt::LaxZonedDateTime, p::DatePeriod)
+    return lzdt + (-p)
+end
+
+function (-)(lzdt::LaxZonedDateTime, p::TimePeriod)
+    return lzdt + (-p)
+end
+
+
+function show(io::IO, lzdt::LaxZonedDateTime)
+    if isvalid(lzdt)
+        print(io, localtime(lzdt))
+
+        if isa(lzdt.zone, NonExistent)
+            print(io, "-DNE")
+        elseif isa(lzdt.zone, Ambiguous)
+            print(io, "-AMB")
+        else
+            print(io, lzdt.zone.offset)
+        end
+    else
+        print(io, "NULL")
+    end
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,62 @@
 using LaxZonedDateTimes
 using Base.Test
+using TimeZones
+import TimeZones: Transition
+import Base.Dates: Day, Hour
+import LaxZonedDateTimes: NonExistent
 
-# write your own tests here
-@test 1 == 2
+t = VariableTimeZone("Testing", [
+    Transition(DateTime(1800,1,1), FixedTimeZone("TST",0,0)),
+    Transition(DateTime(1960,4,1,2), FixedTimeZone("TDT",0,7200)),   # 1960-04-01 02:00
+    Transition(DateTime(1960,4,1,3), FixedTimeZone("TET",0,10800)),  # 1960-04-01 05:00
+    Transition(DateTime(1960,4,1,10), FixedTimeZone("TDT",0,7200)),  # 1960-04-02 12:00
+])
+
+
+@test LaxZonedDateTime(DateTime(1960,3,31,3), t) + Day(1) == LaxZonedDateTime(DateTime(1960,4,1,3), t, NonExistent())
+
+valid_a = LaxZonedDateTime(DateTime(1960,4,1,1), t)
+valid_b = LaxZonedDateTime(DateTime(1960,4,1,6), t)
+non_existent_a = LaxZonedDateTime(DateTime(1960,4,1,2), t, NonExistent())
+non_existent_b = LaxZonedDateTime(DateTime(1960,4,1,3), t, NonExistent())
+null = LaxZonedDateTime()
+@test valid_a + Hour(2) == valid_b
+@test non_existent_a + Hour(1) == null
+@test non_existent_b + Hour(1) == null
+
+ambiguous = LaxZonedDateTime(DateTime(1960,4,1,12),t)
+
+@test non_existent_a + Hour(0) == null
+@test non_existent_b + Hour(0) == null
+@test ambiguous + Hour(0) == null
+
+@test ambiguous - Hour(1) == null
+@test ambiguous + Hour(1) == null
+
+
+wpg = TimeZone("America/Winnipeg")
+lzdt = LaxZonedDateTime(ZonedDateTime(2015,3,7,2, wpg))
+lzdt += Day(1)
+@test lzdt == LaxZonedDateTime(DateTime(2015,3,8,2), wpg, NonExistent())
+lzdt += Day(1)
+@test lzdt == LaxZonedDateTime(ZonedDateTime(2015,3,9,2,wpg))
+
+non_existent = LaxZonedDateTime(DateTime(2015,3,8,2), wpg, NonExistent())
+@test non_existent + Hour(1) == LaxZonedDateTime()
+@test non_existent - Hour(1) == LaxZonedDateTime()
+@test non_existent + Hour(0) == LaxZonedDateTime()
+@test non_existent - Hour(0) == LaxZonedDateTime()
+
+@test (non_existent + Hour(1)) + Hour(1) == LaxZonedDateTime()
+@test (non_existent + Hour(1)) + Day(1) == LaxZonedDateTime()
+
+#=
+using Base.Dates
+using TimeZones
+
+r = ZonedDateTime(2015,3,9,1,wpg):Hour(1):ZonedDateTime(2015,3,10,wpg)
+a = collect(r)
+
+l = map(LaxZonedDateTime, a)
+l - Day(1)
+=#
