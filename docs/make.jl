276ce98d14e326099037f05f5a34b32730cb1848
Chris de Graaf <chris.de.graaf@invenia.ca>
2017-03-23 20:32:04 +0000

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,9 @@ using Documenter, LaxZonedDateTimes
 makedocs(
     modules = [LaxZonedDateTimes],
     format = :html,
-    pages = [],
+    pages = [
+        "Home" => "index.md",
+    ],
     repo = "https://gitlab.invenia.ca/invenia/LaxZonedDateTimes.jl/blob/{commit}{path}#L{line}",
     sitename = "LaxZonedDateTimes.jl",
     authors = "Curtis Vogt, Gem Newman",
