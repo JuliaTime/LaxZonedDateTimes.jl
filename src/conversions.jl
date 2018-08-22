240346f33191ddaea2ca4c67b791adac256a154a
rofinn <rory.finnegan@gmail.com>
2018-08-22 16:28:39 -0500

    Added astimezone support for Interval of LaxZonedDatetime.

--- src/conversions.jl
+++ src/conversions.jl
@@ -5,3 +5,11 @@ function TimeZones.astimezone(zdt::LaxZonedDateTime, tz::TimeZone)
         return LaxZonedDateTime()
     end
 end
+
+function TimeZones.astimezone(i::Interval{LaxZonedDateTime}, tz::TimeZone)
+    return Interval(astimezone(first(i), tz), astimezone(last(i), tz), inclusivity(i))
+end
+
+function TimeZones.astimezone(i::AnchoredInterval{P, LaxZonedDateTime}, tz::TimeZone) where P
+    return AnchoredInterval{P, LaxZonedDateTime}(astimezone(anchor(i), tz), inclusivity(i))
+end
