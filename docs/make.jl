ace2e6bd96a978fa55edfa6146a16085ea7c7daf
Curtis Vogt <curtis.vogt@invenia.ca>
2019-02-20 10:55:02 -0600

    Update Documenter setup

--- docs/make.jl
+++ docs/make.jl
@@ -2,15 +2,17 @@ using Documenter, LaxZonedDateTimes
 
 makedocs(
     modules = [LaxZonedDateTimes],
-    format = :html,
+    format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
     pages = [
         "Home" => "index.md",
     ],
     repo = "https://gitlab.invenia.ca/invenia/LaxZonedDateTimes.jl/blob/{commit}{path}#L{line}",
     sitename = "LaxZonedDateTimes.jl",
     authors = "Curtis Vogt, Gem Newman",
-    assets = ["assets/invenia.css"],
-    strict = true,
+    assets = [
+        "assets/invenia.css"
+    ],
     checkdocs = :none,
+    strict = true,
 )
 
